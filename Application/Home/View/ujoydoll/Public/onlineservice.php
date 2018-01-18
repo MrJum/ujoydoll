f08b126249bf947eb93c420e24da755cc62a0598
daim01 <daim01@mysoft.com.cn>
2018-01-19 00:18:49 +0800

    在线客服

---
+++ Application/Home/View/ujoydoll/Public/onlineservice.php
@@ -0,0 +1,123 @@
+<style type="text/css">
+    /**{margin:0;padding:0;list-style:none;border:none;}*/
+    /*body{font-size:12px;}*/
+    /*a{color:#666;text-decoration:none;}*/
+    /*客服代码部分*/
+    .qqserver .qqserver-header:after,.qqserver .qqserver-header:before,.qqserver li a:after,.qqserver li a:before{display:table;content:' '}
+    .qqserver .qqserver-header:after,.qqserver li a:after{clear:both}
+    .qqserver .qqserver-header,.qqserver li a,.tabs,.user-main,.view-category,.view-category-list>li{*zoom:1}
+    .qqserver{position:fixed;top:50%;right:0;height:209px;margin-top:-104px}
+    .qqserver.unfold .qqserver-body{right:0}
+    .qqserver .qqserver-body{position:absolute;right:-154px;width:132px;padding:12px 10px;-webkit-transition:.3s cubic-bezier(.19,1,.22,1);-o-transition:.3s cubic-bezier(.19,1,.22,1);transition:.3s cubic-bezier(.19,1,.22,1);border:1px solid #62b651;border-radius:4px;background:#F7f9f4}
+    .qqserver .qqserver_fold{position:absolute;right:0;padding:14px 7px;cursor:pointer;border-top-left-radius:4px;border-bottom-left-radius:4px;background:#70ca5d}
+    .qqserver .qqserver-header{padding-bottom:10px;padding-left:6px;border-bottom:1px dashed #d1d4cc}
+    .qqserver .qqserver-header *{float:left}
+    .qqserver .qqserver_arrow{margin-top:-1px;margin-left:7px;cursor:pointer}
+    .qqserver li{margin-top:6px;border-bottom: 1px dashed #d1d4cc}
+    .qqserver li a{display:block;padding:6px 8px 4px;color: #545454;font-family: Arial, Helvetica, sans-serif;}
+    .qqserver li a div{font-size:14px;float:left;margin-right:11px;}
+    .qqserver li a span{font-size:12px;line-height:18px;float:left;text-indent:0;}
+    .qqserver li a span.qqserver-service-alert{font-weight:400;display:block}
+    .qqserver li a:hover{text-decoration:none;border-radius:4px;color:#ff4670}
+    .qqserver li a:hover div{color:#62b651}
+    .qqserver .qqserver-footer{margin-top:10px;padding-top:14px;padding-bottom:14px;padding-left:11px;border-top:1px dashed #d1d4cc}
+    .qqserver .qqserver-footer .text-primary{color:#70CA5D;font-size:14px;}
+    .qqserver .qqserver_icon-alert{display:inline-block;margin-right:10px;vertical-align:-3px;*display:inline;*zoom:1;*vertical-align:-1px}
+    .qqserver-header div{width:90px;height:18px;background-image:url(__PUBLIC__/site/{$Think.THEME_NAME}/onlineservice/images/lanren2.png);background-position:-419px -80px}
+    .qqserver_icon-alert{width:16px;height:14px;background-image:url(__PUBLIC__/site/{$Think.THEME_NAME}/onlineservice/images/lanren2.png);background-position:-595px -85px}
+    .qqserver li a span{width:30px;height:23px;}
+    .qqserver li a .qqserver-service-alert{width:30px;height:22px;background-image:url(__PUBLIC__/site/{$Think.THEME_NAME}/onlineservice/images/lanren2.png);background-position:-342px 0}
+    .qqserver_fold div{width:26px;height:132px;background-image:url(__PUBLIC__/site/{$Think.THEME_NAME}/onlineservice/images/lanren2.png);background-position:0 0}
+    .qqserver_fold:hover div{width:26px;height:132px;background-image:url(__PUBLIC__/site/{$Think.THEME_NAME}/onlineservice/images/lanren2.png);background-position:-27px 0}
+    .qqserver_arrow{width:18px;height:18px;background-image:url(__PUBLIC__/site/{$Think.THEME_NAME}/onlineservice/images/lanren2.png);background-position:-435px 0}
+    .qqserver_arrow:hover{width:18px;height:18px;background-image:url(__PUBLIC__/site/{$Think.THEME_NAME}/onlineservice/images/lanren2.png);background-position:-435px -38px}
+    .qqserver li a i {
+        display: inline-block;
+        width: 20px;
+        height: 20px;
+        margin-right: 6px;
+        vertical-align: middle;
+        background: url(__PUBLIC__/site/{$Think.THEME_NAME}/onlineservice/images/kefuhead.png) no-repeat 0 0;
+        float: left;
+    }
+     .qqserver li a .iskype {
+            background-position: 0 -28px;
+     }
+
+    .qqserver li a .iphone {
+        background-position: 0 -175px;
+    }
+
+    .qqserver li a .imail {
+        background-position: 0 -57px;
+    }
+    .qqserver li a .iwechat {
+        background-position: 0 -276px;
+    }
+
+
+</style>
+
+<!-- 代码部分begin -->
+<div class="qqserver">
+    <div class="qqserver_fold">
+        <div></div>
+    </div>
+    <div class="qqserver-body" style="display: block;">
+        <div class="qqserver-header">
+            <div></div>
+            <span class="qqserver_arrow"></span> </div>
+        <ul>
+            <li> <a title="点击这里给我发消息" href="http://wpa.qq.com/msgrd?v=3&amp;uin=123456789&amp;site=qq&amp;menu=yes" target="_blank">
+                    <i class="iskype"></i>
+                    <span>Nancy</span> </a> </li>
+            <li> <a title="点击这里给我发消息" href="http://wpa.qq.com/msgrd?v=3&amp;uin=123456789&amp;site=qq&amp;menu=yes" target="_blank">
+                    <i class="iskype"></i>
+                    <span>Derrick</span> </a> </li>
+            <li> <a title="点击这里给我发消息" href="http://wpa.qq.com/msgrd?v=3&amp;uin=123456789&amp;site=qq&amp;menu=yes" target="_blank">
+                    <i class="iphone"></i>
+                    <span>+8613570824146</span> </a> </li>
+            <li> <a title="点击这里给我发消息" href="http://wpa.qq.com/msgrd?v=3&amp;uin=123456789&amp;site=qq&amp;menu=yes" target="_blank">
+                    <i class="iphone"></i>
+                    <span>+8613380363476</span> </a> </li>
+            <li> <a title="点击这里给我发消息" href="http://wpa.qq.com/msgrd?v=3&amp;uin=123456789&amp;site=qq&amp;menu=yes" target="_blank">
+                    <i class="iphone"></i>
+                    <span>+8613570824146</span> </a> </li>
+            <li> <a title="点击这里给我发消息" href="http://wpa.qq.com/msgrd?v=3&amp;uin=123456789&amp;site=qq&amp;menu=yes" target="_blank">
+                    <i class="imail"></i>
+                    <span>Eartha</span> </a> </li>
+            <li> <a title="点击这里给我发消息" href="http://wpa.qq.com/msgrd?v=3&amp;uin=123456789&amp;site=qq&amp;menu=yes" target="_blank">
+                    <i class="imail"></i>
+                    <span>Eartha</span> </a> </li>
+            <li style="border-bottom: none"> <a title="点击这里给我发消息" href="http://wpa.qq.com/msgrd?v=3&amp;uin=123456789&amp;site=qq&amp;menu=yes" target="_blank">
+                    <i class="imail"></i>
+                    <span>Eartha</span> </a> </li>
+            <li style="border-bottom: none"> <a title="点击这里给我发消息" href="http://wpa.qq.com/msgrd?v=3&amp;uin=123456789&amp;site=qq&amp;menu=yes" target="_blank">
+                    <i class="iwechat"></i>
+                    <span>Eartha</span> </a> </li>
+        </ul>
+    </div>
+</div>
+
+<script type="text/javascript">
+    $(function(){
+        var $qqServer = $('.qqserver');
+        var $qqserverFold = $('.qqserver_fold');
+        var $qqserverUnfold = $('.qqserver_arrow');
+        $qqserverFold.click(function(){
+            $qqserverFold.hide();
+            $qqServer.addClass('unfold');
+        });
+        $qqserverUnfold.click(function(){
+            $qqServer.removeClass('unfold');
+            $qqserverFold.show();
+        });
+        //窗体宽度小鱼1024像素时不显示客服QQ
+        function resizeQQserver(){
+            $qqServer[document.documentElement.clientWidth < 1024 ? 'hide':'show']();
+        }
+        $(window).bind("load resize",function(){
+            resizeQQserver();
+        });
+    });
+</script>
