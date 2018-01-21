a30395d00fb9ccd0e579f9f08d8972248607b45f
daim01 <daim01@mysoft.com.cn>
2018-01-21 22:26:09 +0800

    222

--- Application/Home/View/ujoydoll/Index/index.php
+++ Application/Home/View/ujoydoll/Index/index.php
@@ -93,48 +93,48 @@
     <div class="blank25"></div>
     <div class="wrap">
         <div id="ismall">
-            <a href="http://t159.web.ueeshop.com/" class="fl" style="overflow:hidden;"><img class="delay" src="__PUBLIC__/site/{$Think.THEME_NAME}/images/e11bf2ca02.jpg"></a>
-            <a href="http://t159.web.ueeshop.com/" class="fr" style="overflow:hidden;"><img class="delay" src="__PUBLIC__/site/{$Think.THEME_NAME}/images/a3bacad888.jpg"></a>
+            <a href="#" class="fl" style="overflow:hidden;"><img class="delay" src="__PUBLIC__/site/{$Think.THEME_NAME}/images/e11bf2ca02.jpg"></a>
+            <a href="#" class="fr" style="overflow:hidden;"><img class="delay" src="__PUBLIC__/site/{$Think.THEME_NAME}/images/a3bacad888.jpg"></a>
         </div>
         <div class="blank25"></div>
         <div class="product_list">
             <div class="item fl ">
                 <div class="inner">
-                    <div class="pic"><a href="http://t159.web.ueeshop.com/sweetheart-floor-length-chiffon-bridesmaids-dresses-p00102p1.html"
+                    <div class="pic"><a href="#"
                                         title="Sweetheart Floor-length Chiffon Bridesmaids Dresses"><img class="delay"
                                                                                                          src="__PUBLIC__/site/{$Think.THEME_NAME}/images/277560a66c.jpg.500x500.jpg"><span></span></a>
                     </div>
                     <div class="blank9"></div>
-                    <div class="name"><a href="http://t159.web.ueeshop.com/sweetheart-floor-length-chiffon-bridesmaids-dresses-p00102p1.html"
+                    <div class="name"><a href="#"
                                          title="Sweetheart Floor-length Chiffon Bridesmaids Dresses">Sweetheart Floor-length Chiffon Bridesmaids Dresses</a></div>
                 </div>
             </div>
             <div class="item fl ">
                 <div class="inner">
-                    <div class="pic"><a href="http://t159.web.ueeshop.com/one-shoulder-sleeveless-ankle-length-satin-bridesmaid-dress-p00009p1.html"
+                    <div class="pic"><a href="#"
                                         title="One Shoulder Sleeveless Ankle-length Satin Bridesmaid Dress"><img class="delay"
                                                                                                                  src="__PUBLIC__/site/{$Think.THEME_NAME}/images/d519b8878e.jpg.500x500.jpg"><span></span></a>
                     </div>
                     <div class="blank9"></div>
-                    <div class="name"><a href="http://t159.web.ueeshop.com/one-shoulder-sleeveless-ankle-length-satin-bridesmaid-dress-p00009p1.html"
+                    <div class="name"><a href="#"
                                          title="One Shoulder Sleeveless Ankle-length Satin Bridesmaid Dress">One Shoulder Sleeveless Ankle-length Satin Bridesmaid Dress</a></div>
                 </div>
             </div>
             <div class="item fl ">
                 <div class="inner">
-                    <div class="pic"><a href="http://t159.web.ueeshop.com/a-four-tiered-white-wedding-cake-p00011p1.html" title="A four-tiered white wedding cake"><img
+                    <div class="pic"><a href="#" title="A four-tiered white wedding cake"><img
                                 class="delay" src="__PUBLIC__/site/{$Think.THEME_NAME}/images/b520299c6c.jpg.500x500.jpg"><span></span></a></div>
                     <div class="blank9"></div>
-                    <div class="name"><a href="http://t159.web.ueeshop.com/a-four-tiered-white-wedding-cake-p00011p1.html" title="A four-tiered white wedding cake">A four-tiered
+                    <div class="name"><a href="#" title="A four-tiered white wedding cake">A four-tiered
                             white wedding cake</a></div>
                 </div>
             </div>
             <div class="item fl no_mar">
                 <div class="inner">
-                    <div class="pic"><a href="http://t159.web.ueeshop.com/only-flowers-of-wedding-hairstyles-p00012p1.html" title="only flowers of Wedding Hairstyles"><img
+                    <div class="pic"><a href="#" title="only flowers of Wedding Hairstyles"><img
                                 class="delay" src="__PUBLIC__/site/{$Think.THEME_NAME}/images/36ee3f7241.jpg.500x500.jpg"><span></span></a></div>
                     <div class="blank9"></div>
-                    <div class="name"><a href="http://t159.web.ueeshop.com/only-flowers-of-wedding-hairstyles-p00012p1.html" title="only flowers of Wedding Hairstyles">only flowers
+                    <div class="name"><a href="#" title="only flowers of Wedding Hairstyles">only flowers
                             of Wedding Hairstyles</a></div>
                 </div>
             </div>
@@ -144,12 +144,12 @@
         <div id="icase">
             <div class="item fl">
                 <div class="cpic">
-                    <div class="inner pic_box"><a href="http://t159.web.ueeshop.com/case/bridesmaid-dresses-c00001c1.html" title="Bridesmaid Dresses"><img
+                    <div class="inner pic_box"><a href="#" title="Bridesmaid Dresses"><img
                                 src="__PUBLIC__/site/{$Think.THEME_NAME}/images/ea4db44b45.jpg.240x240.jpg"></a><em></em></div>
-                    <a href="http://t159.web.ueeshop.com/case/bridesmaid-dresses-c00001c1.html" title="Bridesmaid Dresses" class="frame"></a>
+                    <a href="#" title="Bridesmaid Dresses" class="frame"></a>
                 </div>
                 <div class="text">
-                    <a href="http://t159.web.ueeshop.com/case/bridesmaid-dresses-c00001c1.html" title="Bridesmaid Dresses" class="t">Bridesmaid Dresses</a>
+                    <a href="#" title="Bridesmaid Dresses" class="t">Bridesmaid Dresses</a>
                     <div class="d">A defined waist and full circle skirt flatter the figure in this woven dress. A deep-V neck in the front and back bring sophisticated allure to
                         the youthful sil
                     </div>
@@ -157,14 +157,14 @@
             </div>
             <div class="item fr">
                 <div class="cpic">
-                    <div class="inner pic_box"><a href="http://t159.web.ueeshop.com/case/a-romantic-blush-and-gold-party-in-palm-springs-c00005c1.html"
+                    <div class="inner pic_box"><a href=#"
                                                   title="A Romantic Blush and Gold Party in Palm Springs"><img
                                 src="__PUBLIC__/site/{$Think.THEME_NAME}/images/3b5a8a4028.jpg.240x240.jpg"></a><em></em></div>
-                    <a href="http://t159.web.ueeshop.com/case/a-romantic-blush-and-gold-party-in-palm-springs-c00005c1.html" title="A Romantic Blush and Gold Party in Palm Springs"
+                    <a href="#" title="A Romantic Blush and Gold Party in Palm Springs"
                        class="frame"></a>
                 </div>
                 <div class="text">
-                    <a href="http://t159.web.ueeshop.com/case/a-romantic-blush-and-gold-party-in-palm-springs-c00005c1.html" title="A Romantic Blush and Gold Party in Palm Springs"
+                    <a href="#" title="A Romantic Blush and Gold Party in Palm Springs"
                        class="t">A Romantic Blush and Gold Party in Palm Springs</a>
                     <div class="d">Julie Meyer and Robert "Max" Lavie first spotted one another on the popular dating app Tinder and, once they both swiped right and conducted a
                         full background c
