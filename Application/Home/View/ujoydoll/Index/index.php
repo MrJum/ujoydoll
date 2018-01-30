52193be256dfa0ac2d8d97df168dc46a2709804d
daim01 <daim01@mysoft.com.cn>
2018-01-30 23:14:17 +0800

    222

--- Application/Home/View/ujoydoll/Index/index.php
+++ Application/Home/View/ujoydoll/Index/index.php
@@ -76,10 +76,10 @@
                 </div>
                 <div class="bd"><a href="javascript:;" class="prev"></a><a href="javascript:;" class="next"></a>
                     <ul>
-                        <li style="display: none;"><a href="javascript:void(0);"><img src="__PUBLIC__/site/{$Think.THEME_NAME}/images/slideBox/1.jpg"></a>
+                        <li style="display: none;"><a href="javascript:void(0);"><img src="__PUBLIC__/site/{$Think.THEME_NAME}/images/slideBox/slide01.jpg"></a>
                         </li>
-                        <li style="display: list-item;"><a href="javascript:void(0);"><img src="__PUBLIC__/site/{$Think.THEME_NAME}/images/slideBox/2.jpg"></a></li>
-                        <li style="display: none;"><a href="javascript:void(0);"><img src="__PUBLIC__/site/{$Think.THEME_NAME}/images/slideBox/3.jpg"></a>
+                        <li style="display: list-item;"><a href="javascript:void(0);"><img src="__PUBLIC__/site/{$Think.THEME_NAME}/images/slideBox/slide02.jpg"></a></li>
+                        <li style="display: none;"><a href="javascript:void(0);"><img src="__PUBLIC__/site/{$Think.THEME_NAME}/images/slideBox/slide03.jpg"></a>
                         </li>
                     </ul>
                 </div>
