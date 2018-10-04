2d664090e76d0fce1cb0c594081df2fbabfed157
daim01 <daim01@mysoft.com.cn>
2018-10-05 01:19:09 +0800

    222

---
+++ iiii.sql
@@ -0,0 +1,17 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.7.12 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `yyg_indexdisplay_cont_rel` (
+	`indexdisplay_id` int (10),
+	`cid` int (10)
+); 
+insert into `yyg_indexdisplay_cont_rel` (`indexdisplay_id`, `cid`) values('1','2');
+insert into `yyg_indexdisplay_cont_rel` (`indexdisplay_id`, `cid`) values('1','4');
+insert into `yyg_indexdisplay_cont_rel` (`indexdisplay_id`, `cid`) values('2','1');
+insert into `yyg_indexdisplay_cont_rel` (`indexdisplay_id`, `cid`) values('2','2');
+insert into `yyg_indexdisplay_cont_rel` (`indexdisplay_id`, `cid`) values('2','3');
+insert into `yyg_indexdisplay_cont_rel` (`indexdisplay_id`, `cid`) values('2','4');
